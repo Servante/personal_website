16973bc606086e9c5fdce8a6c99b4be16179e1e2
Wesley <matthew282@msn.com>
2023-02-09 11:20:29 -0700

    debug

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "./controllers/hello_controller.js"
+
+const documentHeight = () => {
+  const doc = document.documentElement
+  doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+}
+
+window.addEventListener('resize', documentHeight)
+documentHeight()
