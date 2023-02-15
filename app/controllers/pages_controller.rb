b2ff7af283263ba787796bc526adafdae9de8ce4
Wesley <matthew282@msn.com>
2023-02-15 11:42:08 -0700

    add #download_pdf function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,4 +11,8 @@ class PagesController < ApplicationController
 
   def blog_page
   end
+
+  def download_pdf
+    send_file "#{Rails.root}/app/assets/docs/Wes_Larson_Resume.pdf", type: "application/pdf", x_sendfile: true
+  end  
 end
